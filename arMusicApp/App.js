49d83f8cb45c41a99b6eec11241087a141af3c34
jesspb <jesspb@bu.edu>
2020-04-30 15:19:11 -0400

    render cube

--- arMusicApp/App.js
+++ arMusicApp/App.js
@@ -1,65 +1,70 @@
 import { AR } from 'expo';
 import { GraphicsView } from 'expo-graphics';
-import { Renderer, THREE } from 'expo-three';
+import ExpoTHREE, { Renderer, THREE, AR as ThreeAR } from 'expo-three';
 import { BackgroundTexture, Camera } from 'expo-three-ar';
 import * as React from 'react';
 import { Platform, View } from 'react-native';
 
-let renderer, scene, camera;
 
-export default function App() {
-  if (Platform.OS !== 'ios') return null;
+export default class App extends React.Component {
+  UNSAFE_componentWillMount() {
+    THREE.suppressExpoWarnings();
+  }
 
-  const onContextCreate = async ({ gl, pixelRatio, width, height }) => {
-    AR.setPlaneDetection(AR.PlaneDetectionTypes.Horizontal);
-
-    renderer = new Renderer({ gl, pixelRatio, width, height });
-
-    scene = new THREE.Scene();
-    scene.background = new BackgroundTexture(renderer);
-
-    camera = new Camera(width, height, 0.01, 1000);
-
-    // Make a cube - notice that each unit is 1 meter in real life, we will make our box 0.1 meters
-    // const geometry = new THREE.BoxGeometry(0.1, 0.1, 0.1);
-    // // Simple color material
-    // const material = new THREE.MeshPhongMaterial({
-    //   color: 0xff00ff,
-    // });
-
-    // Combine our geometry and material
-  //const cube = new THREE.Mesh(geometry, material);
-    // Place the box 0.4 meters in front of us.
-    //cube.position.z = -0.4
-    // Add the cube to the scene
-    //scene.add(cube);
-    // Setup a light so we can see the cube color
-    // AmbientLight colors all things in the scene equally.
-    scene.add(new THREE.AmbientLight(0xffffff));
-  };
-
-  const onResize = ({ scale, width, height }) => {
-    camera.aspect = width / height;
-    camera.updateProjectionMatrix();
-    renderer.setPixelRatio(scale);
-    renderer.setSize(width, height);
-  };
-
-  const onRender = delta => {
-    renderer.render(scene, camera);
-  };
-
-  return (
-    <View style={{ flex: 1 }}>
+  render() {
+    return (
       <GraphicsView
-        style={{ flex: 1 }}
-        onContextCreate={onContextCreate}
-        onRender={onRender}
-        onResize={onResize}
         isArEnabled
-        isArRunningStateEnabled
-        isArCameraStateEnabled
+        onContextCreate={this.onContextCreate}
+        onRender={this.onRender}
       />
-    </View>
-  );
+    );
+  }
+
+  onContextCreate = ({
+    // Web: const gl = canvas.getContext('webgl')
+    gl,
+    width,
+    height,
+    scale,
+  }) => {
+    AR.setPlaneDetection(AR.PlaneDetection.Horizontal);
+
+    // Renderer
+    this.renderer = new Renderer({
+      gl,
+      width,
+      height,
+      pixelRatio: scale,
+    });
+
+    // Scene
+    this.scene = new THREE.Scene();
+    this.scene.background = new BackgroundTexture(this.renderer);
+
+    // Camera
+    this.camera = new Camera(width, height, 0.1, 1000);
+
+    // Cube
+    const geometry = new THREE.BoxGeometry(0.1, 0.1, 0.1);
+
+    const material = new THREE.MeshPhongMaterial({
+      color: 0xff0000,
+    });
+    this.cube = new THREE.Mesh(geometry, material);
+
+    //this.magnetic = new THREE.MagneticObject();
+    // this.magnetic.add(this.cube);
+    this.scene.add(this.cube);
+    // Light
+    this.scene.add(new THREE.AmbientLight(0x404040));
+    this.scene.add(new THREE.DirectionalLight(0xffffff, 0.6));
+  };
+
+  onRender = () => {
+
+    this.renderer.render(this.scene, this.camera);
+  };
 }
+
+const screenCenter = new THREE.Vector2(0.5, 0.5);
