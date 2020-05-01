5238c2ecd165d9954e490838e750eebb5cdf4b1e
jesspb <jesspb@bu.edu>
2020-04-30 22:39:32 -0400

    AR working

--- arMusicApp/App.js
+++ arMusicApp/App.js
@@ -1,9 +1,12 @@
 import { AR } from 'expo';
-import { GraphicsView } from 'expo-graphics';
+import ExpoGraphics, { GraphicsView } from 'expo-graphics';
 import ExpoTHREE, { Renderer, THREE, AR as ThreeAR } from 'expo-three';
 import { BackgroundTexture, Camera } from 'expo-three-ar';
+import * as Permissions from 'expo-permissions';
 import * as React from 'react';
-import { Platform, View } from 'react-native';
+import 'prop-types';
+import 'three';
+
 
 
 export default class App extends React.Component {
@@ -17,6 +20,7 @@ export default class App extends React.Component {
         isArEnabled
         onContextCreate={this.onContextCreate}
         onRender={this.onRender}
+        onResize={this.onResize}
       />
     );
   }
@@ -45,26 +49,61 @@ export default class App extends React.Component {
     // Camera
     this.camera = new Camera(width, height, 0.1, 1000);
 
-    // Cube
-    const geometry = new THREE.BoxGeometry(0.1, 0.1, 0.1);
+    //this.camera = new THREE.PerspectiveCamera(50, width / height, 0.1, 10000);
+    this.camera.position.set(0, 6, 12);
+    this.camera.lookAt(0, 0, 0);
 
-    const material = new THREE.MeshPhongMaterial({
-      color: 0xff0000,
-    });
-    this.cube = new THREE.Mesh(geometry, material);
-
-    //this.magnetic = new THREE.MagneticObject();
-    // this.magnetic.add(this.cube);
-    this.scene.add(this.cube);
     // Light
     this.scene.add(new THREE.AmbientLight(0x404040));
     this.scene.add(new THREE.DirectionalLight(0xffffff, 0.6));
+
+  //  this.setupScene();
+    this.loadModelsAsync();
   };
 
-  onRender = () => {
 
+  loadModelsAsync = async () => {
+    /// Get all the files in the mesh
+    const model = {
+      'model.obj': require('./assets/model.obj'),
+      'materials.mtl': require('./assets/materials.mtl'),
+    };
+
+    /// Load model!
+    const mesh = await ExpoTHREE.loadAsync(
+      [model['model.obj'], model['materials.mtl']],
+      null,
+      name => model[name],
+    );
+
+    this.geometry = model['model.obj'];
+    /// Update size and position
+    ExpoTHREE.utils.scaleLongestSideToSize(mesh, 5);
+    ExpoTHREE.utils.alignMesh(mesh, { y: 1 });
+    /// Smooth mesh
+  //  ExpoTHREE.utils.computeMeshNormals(mesh);
+  mesh.position.y = -2;
+  mesh.position.z = -5;
+    /// Add the mesh to the scene
+    this.scene.add(mesh);
+
+
+    /// Save it so we can rotate
+    this.mesh = mesh;
+  };
+
+  onResize = ({ x, y, scale, width, height }) => {
+
+    this.camera.aspect = width / height;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setPixelRatio(scale);
+    this.renderer.setSize(width, height);
+  };
+
+  onRender = delta => {
+    //this.geometry.rotation.y += 0.4 * delta;
     this.renderer.render(this.scene, this.camera);
   };
 }
 
-const screenCenter = new THREE.Vector2(0.5, 0.5);
+//const screenCenter = new THREE.Vector2(0.5, 0.5);
